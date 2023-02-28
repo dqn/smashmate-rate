2cc84630eda85d85ce39dcb136ab321197322f70
dqn <dqn270@gmail.com>
2023-03-01 08:43:38 +0900

    fix: update

--- src/app/api/rate/[id]/route.ts
+++ src/app/api/rate/[id]/route.ts
@@ -5,10 +5,12 @@ type Params = {
 };
 
 export async function GET(_: Request, { params }: { params: Params }) {
-  const res = await fetch(`https://smashmate.net/user/${params.id}/`);
+  const res = await fetch(`https://smashmate.net/user/${params.id}/`, {
+    cache: "no-store",
+  });
   const text = await res.text();
 
-  const matches = text.match(/<span class="rate_text">.+?(\d{4}).+?<span>/s);
+  const matches = text.match(/<span class="rate_text">.+?(\d{4}).+?<span/s);
 
   if (matches === null) {
     throw new Error("invalid id");
